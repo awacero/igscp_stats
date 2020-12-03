31c153761e5e887cc11f4fe018e13ec3776b68f5
wacero <acerowilson@gmail.com>
2020-12-03 09:08:17 -0500

    add count false picks method in sc3_statistics_utils.py

--- sc3_statistic_utils.py
+++ sc3_statistic_utils.py
@@ -22,7 +22,13 @@ def get_events_by_day(fdsn_client,start_time,end_time):
         
     except Exception as e:
         raise Exception("Error in get_events_by_station_location: %s" %str(e))
-        
+    
+def get_influx_client(host,port,user,passw,db_name):    
+    try:
+        return InfluxDBClient(host=host,port=port, username=user,password=passw,database=db_name)
+    
+    except Exception as e:
+        raise Exception("Error creating influxdb client: %s" %str(e))  
 
 def insert_event_2_influxdb(eventos,client_ifxdb):
     
@@ -33,12 +39,14 @@ def insert_event_2_influxdb(eventos,client_ifxdb):
         if event.magnitudes:
             magnitude=event.preferred_magnitude() or event.magnitudes[0]
             magnitude_value=round(magnitude.mag,4)
-
+        else:
+            magnitude_value=0
+            
         if event.event_type:
             e_type=event.event_type
             event_type=e_type.replace(" ","_")
         else:
-            e_type="not_set"  
+            event_type="not_set"  
         
         event_id=event['resource_id'].id[-13:]
         num_origins= len(event.origins)
@@ -51,39 +59,26 @@ def insert_event_2_influxdb(eventos,client_ifxdb):
                            %(event_id,event_type, lat,lon,magnitude_value,num_origins,mean_picks, time.ns), {'db':'sc3_events_info'},protocol='line' )
         
     
-def get_influx_client(host,port,user,passw,db_name):    
-    try:
-        return InfluxDBClient(host=host,port=port, username=user,password=passw,database=db_name)
+def insert_false_picks(events,client_ifxdb):
     
-    except Exception as e:
-        raise Exception("Error creating influxdb client: %s" %str(e))
-
-
-
-
-
-
+    for i, event in enumerate(events):
+        if event.event_type=="not existing":
+            origin=event.preferred_origin() or event.origins[0]
+            time=origin.time
+            print("Evento falso\n")
+            temp_pick_list=[]
+            for i,pick in enumerate(event.picks):
+                temp_pick_list.append(pick.waveform_id.station_code)
+            temp_pick_set=set(temp_pick_list)
+            for pick in temp_pick_set:
+                print(time,pick,1)
+                client_ifxdb.write("false_event_info,station_name=%s aux_val=1 %s"
+                                   %(pick, time.ns),{'db':'sc3_events_info'},protocol='line')
+            
+            
+            #print("%s\n" %event.origins)
+            
     
-"""
-for i, event in enumerate(eventos):
-    if event.event_type=="not existing":
-        origin=event.preferred_origin() or event.origins[0]
-        time=origin.time
-        print("EVENTO FALSO\n")
-        #print("%s\n"%event.picks)
-        temp_pick_list=[]
-        for i,pick in enumerate(event.picks):
-            temp_pick_list.append(pick.waveform_id.station_code)
-            #print(i,pick.waveform_id.station_code)
-        temp_pick_set=set(temp_pick_list)
-        #print(temp_pick_set)
-        for pick in temp_pick_set:
-            print(time,pick,1)
-        
-        
-        #print("%s\n" %event.origins)
-        
-"""
     
     
     
