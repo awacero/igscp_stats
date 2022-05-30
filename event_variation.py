e202676bb7f523a1a6ef032c72be86ec4da6540c
acerowilson@gmail.com <acerowilson@gmail.com>
2022-05-30 11:27:11 -0500

    got information about network mag and station mags

--- event_variation.py
+++ event_variation.py
@@ -7,6 +7,9 @@ import pandas as pd
 from obspy.io.seiscomp import event 
 from sqlalchemy.sql.expression import column
 
+from bs4 import BeautifulSoup as bs
+
+from obspy import Catalog, UTCDateTime
 import zlib
 
 pd.set_option('display.max_rows',None)
@@ -20,8 +23,15 @@ def zip2catalog(file_path):
     
     zip_buffer = open(file_path, 'rb').read()
     uncompressed = zlib.decompress(zip_buffer)
-    return event._read_sc3ml(uncompressed)
+
+    event_xml = bs(uncompressed,"xml")
+
+    event_info = event_xml.find("event")
+    modification_time = event_info.modificationTime
     
+    temp_event = event._read_sc3ml(uncompressed)[0]
+    temp_event.extra = { 'modification_time' : UTCDateTime(modification_time.get_text()) }
+    return temp_event 
 
 def create_zip_list(event_folder):
 
@@ -32,13 +42,70 @@ def create_zip_list(event_folder):
                 file_list.append(os.path.join(dir_path,zip_file))
     return file_list
 
+
+def modify_event():
+    pass
+
+"""
 def get_network_magnitude(event):
 
     network_magnitude = []
     for m in event.magnitudes:
         network_mag_dict ={'mag':m['mag'],'type':m['magnitude_type'], 'creation_time':m.creation_info['creation_time']}
+        
+        network_magnitude.append(network_mag_dict)
+
+    return network_magnitude
+"""
+
+def get_network_magnitude(event):
+
+    """Get a list of magnitudes """
+
+    network_magnitude = []
+    ##ADD INFORMATION ABOUT EVENT VERSION TO EACH MAGNITUD
+    #event.
+
+    event_id = event.resource_id.id.split("/")[-1]
+
+    '''Recover network magnitudes (average values of all stations)'''
+    for m in event.magnitudes:
+        network_mag_dict ={'mag':m['mag'],'type':m['magnitude_type'], 'creation_time':m.creation_info['creation_time'],
+                            'event_id':event_id, 'modification_time':event.extra['modification_time']}
+        
         network_magnitude.append(network_mag_dict)
 
     return network_magnitude
 
+
+def add_extra_info2station_magnitude(event):
+
+    event_id = event.resource_id.id.split("/")[-1]
+
+    for station_mag in event.station_magnitudes:
+        
+        station_mag.extra =  {'modification_time':event.extra['modification_time'], 'event_id':event_id}
+        print(station_mag.extra)
+
+
+def create_dataframe(event_list):
+
+    temp_list=[]
+
+    for event in event_list:
+        temp_list.append(pd.DataFrame.from_records(event.station_magnitudes ))
+
+    return pd.concat(temp_list)
+
+
+def create_simple_dataframe(event_df):
+
+    event_df['station_id'] = event_df['waveform_id'].apply(lambda x: x.network_code + 
+                                                            "_" + x.station_code + "_" + x.channel_code)
+    event_df['creation_time'] = event_df['creation_info'].apply(lambda x: x.creation_time.datetime)                
+    event_df['author'] = event_df['creation_info'].apply(lambda x: x.author)
+    event_df['modification_time']=event_df['extra'].apply(lambda x: x['modification_time'])
     
+    event_df['event_id']=event_df['extra'].apply(lambda x: x['event_id'])
+
+    return event_df[['station_id','mag','station_magnitude_type','creation_time','author','event_id','modification_time']]
--- run_event_variation.py
+++ run_event_variation.py
@@ -1,106 +1,71 @@
-
+ # -*- coding: utf-8 -*-
 
 import event_variation 
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import sys
 
-events_path = "/home/wacero/borrar/iupd/"
-event_file = "%s/%s" %(events_path,"igepn2022iupd.000030.xml.zip")
-
-file_list = event_variation.create_zip_list(events_path)
 
 event_list = []
+event_network_mag_list =[]
+events_path = "/home/wacero/borrar/iupd/"
+
+
+"""
+Contar el numero de picks de cada version?
+Contar el numero de magnitudes de cada version
+Tomar en cuenta el autor y fecha de creacion de pick y amplitud 
+amplitude: usar? timeWindow? begin, end? SNR? 
+información de ORIGINS: número de orígines por evento, variacion de tiempo origen, lat, lon, depth 
+"""
+
+'''
+Read the event's folder 
+Read the zip files without decompressing
+Read the network magnitudes 
+Transform all data in a dataframe
+'''
+
+
+file_list = event_variation.create_zip_list(events_path)
 
 for file_zip in file_list:
     event_list.append(event_variation.zip2catalog(file_zip))
 
-
-event_single = event_list[10][0]
-print(event_single)
-
-
-event_network_magnitude = event_variation.get_network_magnitude(event_single)
-
-print(event_network_magnitude)
-
-###PROCESS USING PANDAS
+for e in event_list:
+    event_network_mag_list += event_variation.get_network_magnitude(e)
 
 
-station_magnitudes = pd.DataFrame.from_records(event_single.station_magnitudes)
-station_magnitudes['station_id'] = station_magnitudes['waveform_id'].apply(lambda x: x.network_code + "_" + x.station_code + "_" + x.channel_code)
-station_magnitudes['creation_time'] = station_magnitudes['creation_info'].apply(lambda x: x.creation_time)
-#print(station_magnitudes.station_names)
-#print(station_magnitudes.head())
-
-#print(station_magnitudes['mag'],station_magnitudes['station_names'],station_magnitudes['station_magnitude_type'])
-
-station_magnitudes_simple = station_magnitudes[['station_id','mag','station_magnitude_type','creation_time']]
-
-print(station_magnitudes_simple.head())
+for e in event_list:
+    event_variation.add_extra_info2station_magnitude(e)
 
 
+event_df = event_variation.create_dataframe(event_list)
 
-'''
+event_simple = event_variation.create_simple_dataframe(event_df)
 
-station_magnitudes = pd.DataFrame.from_records(event_igepn[0].station_magnitudes)
-
-#print(station_magnitudes)
-
-#for wave in station_magnitudes['waveform_id']:
-#    print(wave.station_code)
-
-station_magnitudes['station_names'] = station_magnitudes['waveform_id'].apply(lambda x: x.station_code)
-
-station_magnitudes['station_names_mag'] = station_magnitudes['station_names'] + station_magnitudes['station_magnitude_type'] 
-station_magnitudes['station_names_mag'].str.replace(' ','')
-#station_magnitudes['station_names_mag'].str.replace('\n','')
-print("CTM")
-print ( station_magnitudes['station_names_mag'][:10] )
-
-station_magnitudes_df = station_magnitudes.reindex(['station_names_mag','mag'])
-#print(station_magnitudes)
-
-#station_magnitudes.plot(x= station_magnitudes['station_names_mag'], y=station_magnitudes['mag'])
+print(event_simple.head(10))
+print(event_network_mag_list[0:10])
 
 
-
-#test= pd.DataFrame([1,2,3], index=['A','B','C'],columns=['num1, num2, num3'])
-
-data =[station_magnitudes['mag'],station_magnitudes['station_names_mag']]
-
-headers = ['mag','station_names_mag']
+''' 
+s_m_s = event_simple.copy()
 
 
-test = pd.concat(data,axis=1, keys=headers)
-#test = pd.DataFrame(station_magnitudes.station_names_mag)
-
-print(test.head())
-
-plt.scatter(test.station_names_mag,test.mag)
-plt.xticks(test.station_names_mag,rotation='vertical')
+for event_net_mag in event_network_mag_list:
+    if event_net_mag['type'] == 'M':
+        print(event_net_mag)
+        plt.axhline(y=event_net_mag['mag'], linestyle='dotted',color='r')
 
 plt.show()
-#print(event)
 
+ax = sns.boxplot(y='mag', x='station_magnitude_type',data=s_m_s,palette='colorblind',hue='station_magnitude_type')
+sns.stripplot(y='mag', x='station_magnitude_type',data=s_m_s,jitter=True,dodge=True, marker='o', alpha=0.5,hue='station_magnitude_type')
+ax.set_xticklabels(ax.get_xticklabels(),rotation = 90)
+ax.legend(bbox_to_anchor=(1,1.0), loc = 'upper left')
+plt.show()
 ''' 
 
 
-"""
-event_igepn = event._read_sc3ml(event_file)
-print(event_igepn)
 
-for ev in event_igepn:
-    
-    print(ev.magnitudes)
-    for mag in ev.magnitudes:
-        print("###")
-        #print(type(mag))
-        print(mag.resource_id())
-    print(ev.origins)
-    for info in ev.origins:
-        print("--")
-        print(info)
-        
-        
-    for station_mag in ev.station_magnitudes:
-        print(station_mag)
-"""
